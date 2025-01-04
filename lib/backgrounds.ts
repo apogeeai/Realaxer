b22cc54b47bf9db04efdbc8cf55301a14c549308
Adam Car <adam@apogeeintelligence.ai>
2025-01-04 02:20:22 -0500

    Added Inital Images

--- lib/backgrounds.ts
+++ lib/backgrounds.ts
@@ -1,9 +1,10 @@
 export const backgrounds = [
-  'https://images.unsplash.com/photo-1434725039720-aaad6dd32dfe', // Tropical waterfall
-  'https://images.unsplash.com/photo-1542273917363-3b1817f69a2d', // Underwater coral reef
-  'https://images.unsplash.com/photo-1483921020237-2ff51e8e4b22', // Snowy mountain forest
-  'https://images.unsplash.com/photo-1448375240586-882707db888b', // Misty rainforest
-  'https://images.unsplash.com/photo-1511497584788-876760111969', // Crystal clear waterfall
+  '/wallhaven-vgv2zm.jpg',
+  '/sw2zerxbdp261.jpg',
+  '/wallhaven-8oxpwj.jpg',
+  '/wallhaven-q6q2yq.jpg',
+  '/izsvveeepp461.jpg',
+  '/Wallpaper-2020.png',
 ] as const;
 
 export type BackgroundType = (typeof backgrounds)[number];
